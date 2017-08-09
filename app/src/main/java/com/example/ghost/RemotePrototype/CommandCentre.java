ed935b20331e83cbd2b0385b47de2173ec775b1c
Ghost <deamonoid@users.noreply.github.com>
2017-08-09 17:59:27 +0530

    Remove button animation

--- app/src/main/java/com/example/ghost/RemotePrototype/CommandCentre.java
+++ app/src/main/java/com/example/ghost/RemotePrototype/CommandCentre.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,8 +13,6 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-
-import ui.ViewProxy;
 
 public class CommandCentre extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,7 +26,6 @@ public class CommandCentre extends AppCompatActivity implements View.OnClickList
 
     private static final int MIN_INTERVAL_TIME = 700;
     private static final int MAX_INTERVAL_TIME = 60000;
-    private RecordButtonUtil mAudioUtil;
 
 
     private float startedDraggingY = -1;
@@ -96,65 +92,6 @@ public class CommandCentre extends AppCompatActivity implements View.OnClickList
                 return true;
             }
         });
-
-        micButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) slideText.getLayoutParams();
-                    params.bottomMargin = dp(30);
-                    slideText.setLayoutParams(params);
-                    ViewProxy.setAlpha(slideText, 1);
-                    startedDraggingY = -1;
-                    startRecord();
-                    micButton.getParent().requestDisallowInterceptTouchEvent(true);
-                    recordPanel.setVisibility(View.VISIBLE);
-                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
-                    startedDraggingY = -1;
-                    stopRecord();
-                } else if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-                    float y = motionEvent.getY();
-                    if (y < -distCanMove) {
-                        stopRecord();
-                    }
-                    y = y + ViewProxy.getY(micButton);
-                    FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) slideText.getLayoutParams();
-                    if (startedDraggingY != -1) {
-                        float dist = (y - startedDraggingY);
-                        params.bottomMargin = dp(30) + (int) dist;
-                        slideText.setLayoutParams(params);
-                        float alpha = 1.0f + dist / distCanMove;
-                        if (alpha > 1) {
-                            alpha = 1;
-                        } else if (alpha < 0) {
-                            alpha = 0;
-                        }
-                        ViewProxy.setAlpha(slideText, alpha);
-                    }
-                    if (y <= ViewProxy.getY(slideText) + slideText.getHeight()
-                            + dp(30)) {
-                        if (startedDraggingY == -1) {
-                            startedDraggingY = y;
-                            distCanMove = (recordPanel.getMeasuredHeight()
-                                    - slideText.getMeasuredHeight() - dp(48)) / 2.0f;
-                            if (distCanMove <= 0) {
-                                distCanMove = dp(80);
-                            } else if (distCanMove > dp(80)) {
-                                distCanMove = dp(80);
-                            }
-                        }
-                    }
-                    if (params.bottomMargin > dp(30)) {
-                        params.bottomMargin = dp(30);
-                        slideText.setLayoutParams(params);
-                        ViewProxy.setAlpha(slideText, 1);
-                        startedDraggingY = -1;
-                    }
-                }
-                view.onTouchEvent(motionEvent);
-                return true;
-            }
-        });
     }
 
     @Override
@@ -193,12 +130,10 @@ public class CommandCentre extends AppCompatActivity implements View.OnClickList
 
     private void startRecord() {
         Toast.makeText(this, "Recording started", Toast.LENGTH_SHORT).show();
-        mAudioUtil.recordAudio();
     }
 
     private void stopRecord() {
         Toast.makeText(this, "Recording stopped", Toast.LENGTH_SHORT).show();
-        mAudioUtil.stopRecord();
     }
 
     public static int dp(float value) {
