f3d23d2c926182d1392c1b6289044609d3764d25
Ghost <deamonoid@users.noreply.github.com>
2017-08-10 15:56:47 +0530

    Add log file

---
+++ app/src/main/java/com/example/ghost/RemotePrototype/AppLog.java
@@ -0,0 +1,15 @@
+package com.example.ghost.RemotePrototype;
+
+import android.util.Log;
+
+/**
+ * Log
+ */
+
+public class AppLog {
+    private static final String APP_TAG = "AudioRecorder";
+
+    public static int logString(String message) {
+        return Log.i(APP_TAG, message);
+    }
+}
