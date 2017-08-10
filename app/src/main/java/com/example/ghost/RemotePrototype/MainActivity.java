4de1cc105eb8abbfc069c3c5e28d5c6a35a538f6
Ghost <deamonoid@users.noreply.github.com>
2017-08-10 16:26:59 +0530

    Remove stacked MainActivity

--- app/src/main/java/com/example/ghost/RemotePrototype/MainActivity.java
+++ app/src/main/java/com/example/ghost/RemotePrototype/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent =  new Intent(MainActivity.this, CommandCentre.class);
         startActivity(intent);
+        finish();
     }
 }
