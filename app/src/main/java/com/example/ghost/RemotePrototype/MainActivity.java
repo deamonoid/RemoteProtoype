9766cfc39cfe0fd8a136c0a880d33f5ef7b02fa0
Ghost <deamonoid@users.noreply.github.com>
2017-08-10 15:57:40 +0530

    Change main screen to CommandCentre

--- app/src/main/java/com/example/ghost/RemotePrototype/MainActivity.java
+++ app/src/main/java/com/example/ghost/RemotePrototype/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent =  new Intent(MainActivity.this, ScanCentre.class);
+        Intent intent =  new Intent(MainActivity.this, CommandCentre.class);
         startActivity(intent);
     }
 }
